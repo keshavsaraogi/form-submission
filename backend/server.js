d1adb4b7575df9cfb215797bdb8c09e0770a3c5b
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2025-04-13 13:10:10 +0530

    session

--- backend/server.js
+++ backend/server.js
@@ -36,9 +36,9 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      secure: false,
+      secure: true,
       maxAge: 1000 * 60 * 60,
-      sameSite: 'lax'
+      sameSite: 'None'
     },
   })
 );
